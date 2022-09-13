717e1b401d3b74c737f8fc517994e7a46b78df55
cloud <khok.abdul@gmail.com>
2022-09-13 20:53:01 +0300

    added checking created/deleted pages + discord webhook connected

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const { Client } = require("@notionhq/client")
-const dotenv = require("dotenv")
+const { Client } = require("@notionhq/client");
+const dotenv = require("dotenv");
+const axios = require('axios');
+const qs = require('qs');
 dotenv.config();
 
 const notion = new Client({
@@ -8,13 +10,89 @@ const notion = new Client({
 
 const database_id = process.env.NOTION_DATABASE_ID;
 
-(async () => {
-    const allPages = await notion.search({
-        query: '',
-        filter: {
-            value: 'page',
-            property: 'object',
-        },
-    });
-    console.log(allPages.results);
-})()
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+let previousPages;
+
+const differ = (prev, curr) => {
+    const deletedPages = prev.reduce((acc, currentPage) => {
+        if (!curr.find(item => item.id === currentPage.id)) {
+            return [
+                ...acc,
+                {
+                    id: currentPage.id,
+                }
+            ];
+        } else {
+            return acc;
+        }
+    }, [])
+
+    const createdPages = curr.reduce((acc, currentPage) => {
+        if (!prev.find(item => item.id === currentPage.id)) {
+            return [
+                ...acc,
+                {
+                    id: currentPage.id,
+                }
+            ];
+        } else {
+            return acc;
+        }
+    }, [])
+
+    return {
+        deletedPages,
+        createdPages,
+    }
+};
+
+setInterval(() => {
+    (async () => {
+        const allPages = await notion.search({
+            query: '',
+            filter: {
+                value: 'page',
+                property: 'object',
+            },
+        });
+
+        const currentPages = allPages.results
+            .map(page => ({
+                id: page.id,
+                url: page.url,
+            }));
+
+        if (previousPages) {
+            const diff = differ(previousPages, currentPages);
+
+            if (diff.createdPages.length) {
+                axios({
+                    method: 'post',
+                    url: webhookUrl,
+                    data: qs.stringify({
+                        content: `Pages ${JSON.stringify(diff.createdPages)} was created`
+                    }),
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    }
+                });
+            }
+
+            if (diff.deletedPages.length) {
+                axios({
+                    method: 'post',
+                    url: webhookUrl,
+                    data: qs.stringify({
+                        content: `Pages ${JSON.stringify(diff.deletedPages)} was deleted`
+                    }),
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    }
+                });
+            }
+        }
+
+        previousPages = currentPages;
+    })();
+}, 1000)
